e175e49def4b3b46c61d4d531268eceb580beca8
khoramian <khoramian8@gmail.com>
2021-05-07 07:42:53 -0400

    Initial commit

---
+++ client_config.py
@@ -0,0 +1,6 @@
+class ClientConfig(object):
+    APP_NAME = 'p2'
+    COMPANY_NAME = 'Company 1'
+    # Not a real public key
+    # PUBLIC_KEY = ('dk3lkd8dslk38383lsls8u88sl')
+    UPDATE_URLS = ['https://github.com/khoramian/p2']
